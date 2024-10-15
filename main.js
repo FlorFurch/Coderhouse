ed84cba9358ae30539d91b7bc89c049b805db0f5
Florencia Furch <37964182+FlorFurch@users.noreply.github.com>
2024-10-15 20:45:55 -0300

    Add files via upload
    Proyecto final JS Coderhouse

--- main.js
+++ main.js
@@ -1,77 +1,132 @@
 /*
 1. Stock de mercadería
 2. Crear un constructor para los nuevos productos
-3. Subir productos nuevos 
-          ---------------------- mediante un Form
-4. Buscar un producto 
-----------------------con un Input
-5. LocalStorage
-
+3. Subir productos nuevos mediante un Form
+4. Buscar un producto por ID
+5. Abrir "Ver todos los productos en stock"
 */
 
 
 
 // 1. Stock de mercadería
-let baseDatosMercaderia = [
-    { id: "123", tipo: "remera", talle: "M", cantidad: 2, precioCosto: 5200 },
-    { id: "123", tipo: "remera", talle: "L", cantidad: 2, precioCosto: 5200 },
-    { id: "A456", tipo: "buzo", talle: "L", cantidad: 5, precioCosto: 16000 },
-    { id: "B789", tipo: "pantalón", talle: "S", cantidad: 1, precioCosto: 18000 },
-]
-
+let baseDatosMercaderia = []
+fetch('https://fakestoreapi.com/products/')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(post => {
+            let id = post.id;
+            let tipo = post.title;
+            let talle = post.category;
+            let precioCosto = post.price;
+            let precioTotal = precioCosto * 2;
+            const producto = { id, tipo, talle, precioCosto, precioTotal }
+            baseDatosMercaderia.push(producto)
+            //Id y tipo de producto guardado en localstorage
+            localStorage.setItem(id, tipo)
+            let usuario = localStorage.getItem(id)
+            //listado de productos
+            let listaDeProductos = document.querySelector(`#modal`)
+            let tabla = document.createElement("div")
+            tabla.innerHTML = `<table>
+                          <tr>
+                          <td class="td-id">${post.id}</td>
+                          <td class="td-tipo">${post.title}</td>
+                          <td class="td-categoria">${post.category}</td>
+                          <td class="td-costo">${post.price}</td>
+                          <td class="td-total">${precioTotal}</td>
+                          </tr>
+                          </table>`
+            listaDeProductos.appendChild(tabla)
+            return tabla
+        })
+    })
 
 // 2. Constructor de productos
 // 3. Productos ingresados en el Form
 function formularioDeCarga(e) {
+    const cargaNuevoProducto = document.querySelector("#formulario-carga");
     e.preventDefault();
     let id = document.querySelector("#id-carga").value
     let tipo = document.querySelector("#tipo-carga").value
-    let talle = document.querySelector("#talle-carga").value
-    let cantidad = Number(document.querySelector("#cantidad-carga").value)
+    let categoria = document.querySelector("#talle-carga").value
     let precioCosto = Number(document.querySelector("#precio-carga").value)
-    const producto = { id, tipo, talle, cantidad, precioCosto }
-    baseDatosMercaderia.push(producto)
-    //return baseDatosMercaderia;
-    console.table(baseDatosMercaderia)
-
+    let precioTotal = precioCosto * 2;
+    const producto = { id, tipo, categoria, precioCosto }
+    baseDatosMercaderia.push(producto);
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: `El producto ${producto.tipo} fue cargado con éxito`,
+        showConfirmButton: false,
+        timer: 2500
+    });
+    let listaDeProductos = document.querySelector(`#modal`)
+    let tabla = document.createElement("div")
+    tabla.innerHTML = `<table>
+                          <tr>
+                          <td class="td-id">${producto.id}</td>
+                          <td class="td-tipo">${producto.tipo}</td>
+                          <td class="td-categoria">${producto.categoria}</td>
+                          <td class="td-costo">${producto.precioCosto}</td>
+                          <td class="td-total">${precioTotal}</td>
+                          </tr>
+                          </table>`
+    listaDeProductos.appendChild(tabla)
+    cargaNuevoProducto.reset();
 }
-
 const cargaNuevoProducto = document.querySelector("#formulario-carga");
 cargaNuevoProducto.addEventListener("submit", formularioDeCarga);
 
-
-
-
 // 4. Buscar un producto por ID
 function validarForm(e) {
+    const busquedaID = document.querySelector("#formulario-busqueda");
     e.preventDefault();
     let IDbuscado = document.querySelector("#id-busqueda").value.toUpperCase();
-    let resultado = baseDatosMercaderia.filter((producto) => producto.id.toUpperCase().includes(IDbuscado))
+    let resultado = baseDatosMercaderia.filter((producto) => producto.id.toString().toUpperCase().includes(IDbuscado))
     if (resultado.length > 0) {
+        const espacio = document.getElementById("tabla-resultados")
+        let tabla = document.createElement("div")
+        tabla.innerHTML = `<table>
+                       <thead>
+                      <tr>
+                      <th class="td-id">ID</td>
+                      <th class="td-tipo">Tipo de producto</th>
+                      <th class="td-categoria">Categoría</th>
+                      <th class="td-costo">Costo</th>
+                      <th class="td-total">Precio</th>
+                      </tr>
+                      </thead>
+                      </table>`
+        espacio.appendChild(tabla)
         for (const busqueda of resultado) {
             const espacio = document.getElementById("tabla-resultados")
             let tabla = document.createElement("div")
+            precioTotal = busqueda.precioCosto * 2
             tabla.innerHTML = `<table>
                           <tr>
-                          <td>${busqueda.id}</td>
-                          <td>${busqueda.tipo}</td>
-                          <td>${busqueda.talle}</td>
-                          <td>${busqueda.precioCosto}</td>
+                          <td class="td-id">${busqueda.id}</td>
+                          <td class="td-tipo">${busqueda.tipo}</td>
+                          <td class="td-costo">${busqueda.categoria}</td>
+                          <td class="td-costo">${busqueda.precioCosto}</td>
+                          <td class="td-total">${precioTotal}</td>
                           </tr>
                           </table>`
             espacio.appendChild(tabla)
         }
-
     }
+    busquedaID.reset();
 }
-
 const busquedaID = document.querySelector("#formulario-busqueda");
 busquedaID.addEventListener("submit", validarForm);
+busquedaID.reset();
 
-
-// 5. LocalStorage
-localStorage.setItem("mensaje", "Utilice este programa para subir y/o buscar productos")
-let mensaje = localStorage.getItem("mensaje")
-alert(mensaje)
-
-formularioDeCarga
+// 5. Mostrar todos los productos en stock
+const abrirVentana = document.querySelector(`#ver-todos`);
+const cerrarVentana = document.querySelector(`#cierre`)
+const ventana = document.querySelector(`#modal`);
+abrirVentana.addEventListener(`click`, () => {
+    ventana.showModal();
+})
+cerrarVentana.addEventListener(`click`, () => {
+    ventana.close();
+})
---
+++ readme.md
@@ -0,0 +1,13 @@
+SISTEMA DE STOCK:
+
+1- Muestra los productos almacenados en una base de datos.
+2- Cuenta con formulario de carga de nuevos productos.
+3- Búsqueda de mercadería por su ID (pude ser letras, números o una mezcla de ambos).
+4- El sistema calcula el precio de venta del producto. 
+
+
+
+
+Recursos:
+
+En esta aplicación se utilizó SweetAlert2 y Fake Store API (https://fakestoreapi.com/).
